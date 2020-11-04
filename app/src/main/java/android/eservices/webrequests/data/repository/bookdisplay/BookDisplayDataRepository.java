d803909a99b0a41a8105917efab92f5d74007de9
Fliro59 antoine <antoine.meresse@orange.fr>
2020-11-04 17:20:29 +0100

    BookDisplayRepository, BookDisplayDataRepository : ajout 3 méthodes pour les favoris

--- app/src/main/java/android/eservices/webrequests/data/repository/bookdisplay/BookDisplayDataRepository.java
+++ app/src/main/java/android/eservices/webrequests/data/repository/bookdisplay/BookDisplayDataRepository.java
@@ -1,21 +1,46 @@
 package android.eservices.webrequests.data.repository.bookdisplay;
 
 import android.eservices.webrequests.data.api.model.BookSearchResponse;
+import android.eservices.webrequests.data.database.BookEntity;
+import android.eservices.webrequests.data.repository.bookdisplay.local.BookDisplayLocalDataSource;
 import android.eservices.webrequests.data.repository.bookdisplay.remote.BookDisplayRemoteDataSource;
 
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.reactivex.Single;
 
 // Demander au local ou au remote
 public class BookDisplayDataRepository implements BookDisplayRepository {
 
     private BookDisplayRemoteDataSource bookDisplayRemoteDataSource;
+    private BookDisplayLocalDataSource bookDisplayLocalDataSource;
 
-    public BookDisplayDataRepository(BookDisplayRemoteDataSource bookDisplayRemoteDataSource){
+    public BookDisplayDataRepository(BookDisplayRemoteDataSource bookDisplayRemoteDataSource, BookDisplayLocalDataSource bookDisplayLocalDataSource){
         this.bookDisplayRemoteDataSource = bookDisplayRemoteDataSource;
+        this.bookDisplayLocalDataSource = bookDisplayLocalDataSource;
     }
 
     @Override
     public Single<BookSearchResponse> getBookSearchResponse(String keywords) {
         return bookDisplayRemoteDataSource.searchBooks(keywords);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public Flowable<List<BookEntity>> getFavoriteBooks() {
+        return null;
+    }
+
+    @Override
+    public Completable insertBook(String id) {
+        return null;
+    }
+
+    @Override
+    public Completable deleteBook(String id) {
+        return null;
     }
 }
--- app/src/main/java/android/eservices/webrequests/data/repository/bookdisplay/BookDisplayRepository.java
+++ app/src/main/java/android/eservices/webrequests/data/repository/bookdisplay/BookDisplayRepository.java
@@ -1,11 +1,20 @@
 package android.eservices.webrequests.data.repository.bookdisplay;
 
 import android.eservices.webrequests.data.api.model.BookSearchResponse;
+import android.eservices.webrequests.data.database.BookEntity;
 
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.reactivex.Single;
 
 public interface BookDisplayRepository {
-
+    // Remote
     Single<BookSearchResponse> getBookSearchResponse(String keywords);
 
+    // Local
+    Flowable<List<BookEntity>> getFavoriteBooks();
+    Completable insertBook(String id);
+    Completable deleteBook(String id);
 }
